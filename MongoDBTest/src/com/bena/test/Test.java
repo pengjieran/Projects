a301c0d1c5d269f40a11ced7c352f67f884ab7d4
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-07 14:59:54 +0800

    MongoDB操作的部分方法测试

--- MongoDBTest/src/com/bena/test/Test.java
+++ MongoDBTest/src/com/bena/test/Test.java
@@ -1,11 +1,10 @@
 package com.bena.test;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 
 public class Test {
@@ -21,6 +20,7 @@ public class Test {
 	    
 	    DBCollection collection = TestConfig.getCollection(host, port, dbName, docName);
 	   
+	    //插入一百条测试数据
 	    for (int i = 0; i < 100; i++) {
 		
 		BasicDBObject object = new BasicDBObject("name", "user" + i).append("age", i).append("sex", i/2 == 0 ? "男" : "女");
@@ -28,7 +28,21 @@ public class Test {
 	    }
 	    
 	    System.out.println(collection.find().count());
-	    collection.drop();
+	    //删除表
+	    //collection.drop();
+	    
+	    //查询出第一条记录
+	    DBObject object = collection.findOne();
+	    System.out.println(object.toString());
+	    
+	    //查询出所有记录
+	    DBCursor cursor = collection.find();
+	    while (cursor.hasNext()) {
+		
+		DBObject dbObject = cursor.next();
+		System.out.println(dbObject.toString());
+		
+	    }
 	    
 	} catch (UnknownHostException e) {
 	    
