31ccdf99ee19722ee3dc876cfdbaad0914e4e54e
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-09 18:33:48 +0800

    新增内容： 查询数据库相关信息：是否自动提交，事物级别，是否只读，数据库相关信息等

--- jdbctest/src/com/bean/DBTest.java
+++ jdbctest/src/com/bean/DBTest.java
@@ -1,7 +1,11 @@
 package com.bean;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -23,6 +27,7 @@ public class DBTest {
 		}
 	}
 	
+	@SuppressWarnings("unused")
 	@Test
 	public void getdbInfo() {
 		try {
@@ -30,6 +35,38 @@ public class DBTest {
 			Connection connection = TestUtils.getConnection();
 			String catalog = connection.getCatalog();
 			System.out.println(catalog);
+			
+			//是否自动提交
+			boolean autoCommit = connection.getAutoCommit();
+			System.out.println(autoCommit);
+			
+			//事物级别
+			int transactionIsolation = connection.getTransactionIsolation();
+			System.out.println(transactionIsolation);
+			
+			//未知的一个功能
+			Map<String, Class<?>> typeMap = connection.getTypeMap();
+			Set<String> keySet = typeMap.keySet();
+			for (String type : keySet) {
+				
+				System.out.println(type);
+			}
+			
+			//获取客户端信息，不是太常用
+			Properties clientInfo = connection.getClientInfo();
+			
+			//该数据库是否只读
+			boolean readOnly = connection.isReadOnly();
+			System.out.println(readOnly);
+			
+			//数据库相关信息,只写部分，其它不再一一列举
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+			System.out.println(databaseMetaData.getIdentifierQuoteString());
+			System.out.println(databaseMetaData.getDatabaseProductName());
+			
+			String schema = connection.getSchema();
+			System.out.println(schema);
+			
 		} catch (ClassNotFoundException | SQLException e) {
 			
 			e.printStackTrace();
