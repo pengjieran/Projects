711b347d2c4c9d79596889c24be4b3ad443649ed
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-09 17:31:58 +0800

    创建jdbc的测试项目，主要目的是测试关系型数据库的操作方式，搭建开发环境，采用junit进行测试

---
+++ jdbctest/src/com/bean/TestUtils.java
@@ -0,0 +1,18 @@
+package com.bean;
+
+import com.mysql.jdbc.Connection;
+
+/**
+ * 提供数据库连接，创建初始化数据的文件
+ * @author Dean
+ *
+ */
+public class TestUtils {
+	
+	private TestUtils() {}
+	
+	public static Connection getConnection() {
+		
+		return null;
+	}
+}
