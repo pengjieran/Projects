5b64c12f005f7708afd16a0b294164f79666238d
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-15 15:16:57 +0800

    增加注释已说明各种handle的不同之处

--- ExtendPackageTest/src/com/bean/dbutils/DBUtilsTest.java
+++ ExtendPackageTest/src/com/bean/dbutils/DBUtilsTest.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ArrayHandler;
+import org.apache.commons.dbutils.handlers.ArrayListHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.MapHandler;
@@ -49,6 +50,18 @@ public class DBUtilsTest {
     
     @Test
     public void query() throws SQLException {
+	
+	/**
+	 *ArrayHandler：把结果集中的第一行数据转成对象数组。
+	 *ArrayListHandler：把结果集中的每一行数据都转成一个对象数组，再存放到List中。
+	 *BeanHandler：将结果集中的第一行数据封装到一个对应的JavaBean实例中。
+	 *BeanListHandler：将结果集中的每一行数据都封装到一个对应的JavaBean实例中，存放到List里。
+	 *ColumnListHandler：将结果集中某一列的数据存放到List中。
+ 	 *KeyedHandler：将结果集中的每一行数据都封装到一个Map里，然后再根据指定的key把每个Map再存放到一个Map里。
+ 	 *MapHandler：将结果集中的第一行数据封装到一个Map里，key是列名，value就是对应的值。
+ 	 *MapListHandler：将结果集中的每一行数据都封装到一个Map里，然后再存放到List。
+ 	 *ScalarHandler：将结果集中某一条记录的其中某一列的数据存成Object。
+	 */
 
 	//采用MapHandler存储方式查询
 	MapHandler handler = new MapHandler();
@@ -96,9 +109,10 @@ public class DBUtilsTest {
 	Object[] objects = runner.query(connection, "select * from users where age > ?", arrayHandler, new Object[]{"1005"});
 	for (Object object : objects) {
 	    
-	    
 	    System.out.println(object.toString());
 	}
+	
+	ArrayListHandler arrayListHandler = new ArrayListHandler();
     }
     
     @Test
