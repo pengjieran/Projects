44ffeb8986fe9f4d03ae940cc30f49e13fe153a5
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-18 20:11:07 +0800

    修改部分修正

--- WebExample/src/com/bean/servlet/LoginServlet.java
+++ WebExample/src/com/bean/servlet/LoginServlet.java
@@ -36,7 +36,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
