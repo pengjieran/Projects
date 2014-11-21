52f0f13439ca402ea88e83614914ad497269e337
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-21 18:26:31 +0800

    使用jndi的方式从服务器数据源中拿到一个数据库连接

--- WebExample/src/com/bean/servlet/RegisterServlet.java
+++ WebExample/src/com/bean/servlet/RegisterServlet.java
@@ -1,11 +1,20 @@
 package com.bean.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class RegisterServlet
@@ -28,6 +37,24 @@ public class RegisterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		System.out.println("跳转过来的输出语句");
+		
+		Context context = null;
+		try {
+			
+			context = new InitialContext();
+			DataSource dataSource = (DataSource) context.lookup("java:jboss/datasources/mysqlDS");
+			Connection connection = dataSource.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from users");
+			System.out.println(resultSet.getRow());
+			
+		} catch (NamingException e) {
+			
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 	/**
