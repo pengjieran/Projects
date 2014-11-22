5c6172e64f8b5c22464826f836c1e6349b9def88
DeanAaron <pjr0206@163.com>
2014-11-22 19:31:19 +0800

    test printf and scanf

---
+++ testc/testprint.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int a;
+    a = 5;
+    printf("%d", a);
+    system("pause");
+    printf("请输入一个数字：");
+    int b = 0;
+    scanf("%d", &b);
+    printf("%d", b);
+    system("pause");
+    return 0;
+}
