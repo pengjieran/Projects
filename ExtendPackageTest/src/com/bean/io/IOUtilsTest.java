90ed3307b88adb2d9c43e798049e4fa33386610c
Jieran.Peng <DeanAaron@users.noreply.github.com>
2014-11-25 11:45:19 +0800

    添加读取文件的各种方式

--- ExtendPackageTest/src/com/bean/io/IOUtilsTest.java
+++ ExtendPackageTest/src/com/bean/io/IOUtilsTest.java
@@ -2,6 +2,7 @@ package com.bean.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -32,15 +33,64 @@ public class IOUtilsTest {
     }
     
     @Test
-    public void test() {
-	
-	File file = new File("");
-	String absolutePath = file.getAbsolutePath();
-	String filePath = absolutePath + "\\src\\com\\bean\\io\\test\\test1\\test2\\sbt.txt";
-	System.out.println(absolutePath);
-	long size = FileUtils.sizeOf(new File(absolutePath + "\\src\\com\\bean\\io\\test\\test1\\test2\\sbt.txt"));
-	System.out.println(absolutePath + "\\src\\com\\bean\\io\\test");
-	System.out.println(size);
+    public void testFilePath() {
+    	
+    	//
+		File file = new File("");
+		String absolutePath = file.getAbsolutePath();
+		String filePath = absolutePath + "\\src\\com\\bean\\io\\test\\test1\\test2\\sbt.txt";
+		System.out.println(absolutePath);
+		long size = FileUtils.sizeOf(new File(absolutePath + "\\src\\com\\bean\\io\\test\\test1\\test2\\sbt.txt"));
+		System.out.println(absolutePath + "\\src\\com\\bean\\io\\test");
+		System.out.println(size);
+		
+		//获取类加载的根路径
+		File f = new File(this.getClass().getResource("/").getPath());
+		System.out.println(f);
+		
+		//获取当前类所在工程路径
+		File f2 = new File(this.getClass().getResource("").getPath());
+		System.out.println(f2);
+		
+		//获取项目路径
+		File f3 = new File("");
+		try {
+			
+			String canonicalPath = f3.getCanonicalPath();
+			System.out.println(canonicalPath);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		//
+		URL resource = this.getClass().getClassLoader().getResource("");
+		System.out.println(resource);
+		
+		//
+		String property = System.getProperty("user.dir");
+		System.out.println(property);
+		
+		String string = System.getProperty("java.class.path");
+		System.out.println(string);
+    }
+    
+    @Test
+    public void readFile() {
+    	
+    	File file = new File("");
+    	String absolutePath = file.getAbsolutePath();
+    	String filePath = absolutePath + "\\src\\com\\bean\\io\\test\\test1\\test2\\sbt.txt";
+    	try {
+    		
+			byte[] readFileToByteArray = FileUtils.readFileToByteArray(new File(filePath));
+			
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+    	
     }
 
 }
