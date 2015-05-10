15e00822c27accb6c7a6b9d97acc6e935390f7f2
Aaron <pjr0206@163.com>
2015-05-10 22:54:28 +0800

    修正文件分隔符

--- javacv-example/src/com/main/test/VideoCompress.java
+++ javacv-example/src/com/main/test/VideoCompress.java
@@ -1,8 +1,17 @@
 package com.main.test;
 
+import java.io.File;
+
+
 public class VideoCompress {
 
 	public static void main(String[] args) {
 		
+		String pathProperty = System.getProperty("file.separator");
+		String filePath = "test" + pathProperty + "resources" + pathProperty + "videos" + pathProperty + "output.mp4";
+		File file = new File(filePath);
+		System.out.println(filePath);
+		System.out.println(file.exists());
+		//FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("test" +);
 	}
 }
